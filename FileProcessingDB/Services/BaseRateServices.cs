3917ed60583ce2b275308e133ec76a6e3e612464
Mykola Kuchirka <mykolakuchirka@gmail.com>
2021-07-09 16:58:43 +0300

    Data returned using EF

--- FileProcessingDB/Services/BaseRateServices.cs
+++ FileProcessingDB/Services/BaseRateServices.cs
@@ -22,32 +22,8 @@ namespace FileProcessingDB.Services
 		}
 		
 		public IEnumerable<BaseRate> GetAll()
-		{
-			var baseRate = _database.BaseRates.ToList();
-			var Advertisers = _database.Advertisers.ToList();
-			var Amounts = _database.Amounts.ToList();
-			var CreditScores = _database.CreditScores.ToList();
-			var Ltvs = _database.ltvs.ToList();
-			var ProductTypes = _database.ProductTypes.ToList();
-			var States = _database.States.ToList();
-			var dataToReturn = baseRate.Select(a => new BaseRate
-			{
-				Value = a.Value,
-				TotalTerm = a.TotalTerm,
-				LastModified = a.LastModified,				
-				IDAdv = a.IDAdv,
-				IDAm = a.IDAm,
-				IDCr = a.IDCr,
-				IDL = a.IDL,
-				IDPr = a.IDPr,
-				IDSt = a.IDSt,
-				Advertiser = Advertisers.First(o => o.Id == a.IDAdv),
-				Amount = Amounts.First(o => o.Id == a.IDAm),
-				CreditScore = CreditScores.First(o => o.Id == a.IDCr),
-				Ltv = Ltvs.First(o => o.Id == a.IDL),
-				ProductType = ProductTypes.First(o => o.Id == a.IDPr),
-				State = States.First(o => o.Id == a.IDSt)
-			});
+		{						
+			var dataToReturn = _database.BaseRates.ToArray();
 			return dataToReturn;
 		}
 		public int CountCurrEl()
